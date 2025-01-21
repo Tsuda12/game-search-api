f3b4f078e745bff13dc269c4bd41a698fa11c3d0
Felipe Tsuda <felipe.tsuda@pecaai.com.br>
2025-01-21 09:07:36 -0300

    init: start project

---
+++ src/main/java/br/com/tsuda/game_search_api/GameSearchApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.tsuda.game_search_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameSearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameSearchApiApplication.class, args);
+	}
+
+}
